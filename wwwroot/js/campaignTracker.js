a66903abf3b1aa88a70854edb9ef14db88a3bfe1
LL-99 <lucas.leder99@gmail.com>
2026-05-09 13:32:16 +0200

    save & load buttons

---
+++ wwwroot/js/campaignTracker.js
@@ -0,0 +1,34 @@
+window.campaignTracker = {
+    async saveTextFile(fileName, text, contentType) {
+        if (!window.showSaveFilePicker) {
+            return false;
+        }
+
+        const handle = await window.showSaveFilePicker({
+            suggestedName: fileName,
+            types: [
+                {
+                    description: "JSON file",
+                    accept: { [contentType || "application/json"]: [".json"] }
+                }
+            ]
+        });
+
+        const writable = await handle.createWritable();
+        await writable.write(new Blob([text], { type: contentType || "application/json" }));
+        await writable.close();
+
+        return true;
+    },
+
+    openFileInput(inputId) {
+        const input = document.getElementById(inputId);
+
+        if (!input) {
+            return;
+        }
+
+        input.value = "";
+        input.click();
+    }
+};
